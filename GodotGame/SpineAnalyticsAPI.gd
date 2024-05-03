9dd5d7bdd2024bf807ac6bea48aa5d65ef9ef0e9
Shaun Orssaud <72015533+Shorssaud@users.noreply.github.com>
2024-05-03 12:23:40 +0900

    ADD Godot game analytics

--- GodotGame/SpineAnalyticsAPI.gd
+++ GodotGame/SpineAnalyticsAPI.gd
@@ -1,72 +1,97 @@
 extends Node
-
-# Define custom data structures
-class_name SpineAnalyticsAPI
-
-# Define ErrorData structure
 class ErrorData:
 	var title: String
 	var description: String
 	var log: String
 	var timestamp: String
 
-	func _init():
-		timestamp = OS.get_datetime().to_string()
+	# Class method to return dictionary of class properties
+	func get_serializable_properties() -> Dictionary:
+		var properties = {
+			"title": title,
+			"description": description,
+			"log": log,
+			"timestamp": timestamp
+		}
+		return properties
 
-# Define AchievementData structure
 class AchievementData:
 	var title: String
 	var description: String
 	var timestamp: String
 
-	func _init():
-		timestamp = OS.get_datetime().to_string()
+	# Class method to return dictionary of class properties
+	func get_serializable_properties() -> Dictionary:
+		var properties = {
+			"title": title,
+			"description": description,
+			"timestamp": timestamp
+		}
+		return properties
 
-# Define StatisticData structure
 class StatisticData:
 	var title: String
 	var description: String
-	var value: Variant # Accepts either string or number
+	var value: String
 	var timestamp: String
 
-	func _init():
-		timestamp = OS.get_datetime().to_string()
+	# Class method to return dictionary of class properties
+	func get_serializable_properties() -> Dictionary:
+		var properties = {
+			"title": title,
+			"description": description,
+			"value": value,
+			"timestamp": timestamp
+		}
+		return properties
 
-# Define GameSessionData structure
 class GameSessionData:
 	var gameId: String
-	var statistics: Array
-	var achievements: Array
-	var errors: Array
+	var statistics: Array = []
+	var achievements: Array = []
+	var errors: Array = []
 
-	func _init():
-		statistics = []
-		achievements = []
-		errors = []
+	# Class method to return dictionary of class properties
+	func get_serializable_properties() -> Dictionary:
+		var properties = {
+			"gameId": gameId,
+			"statistics": statistics,
+			"achievements": achievements,
+			"errors": errors
+		}
+		return properties
 
-# Define SpineAnalyticsAPI class
-class SpineAnalyticsAPI:
-	var baseUrl: String = "https://app-ekrmeoi24a-uc.a.run.app"
-	var apiKey: String
-	var gameId: String
-	var gameSessionData: GameSessionData
+class SessionResponse:
+	var sessionId: String
 
-	func initialize(_apiKey: String, _gameId: String) -> void:
-		apiKey = _apiKey
-		gameId = _gameId
-		gameSessionData = GameSessionData.new()
+var baseUrl: String = "https://app-ekrmeoi24a-uc.a.run.app"
+var apiKey: String
+var sessionId: String
+var gameSessionData: GameSessionData = GameSessionData.new()
 
-	func send_session(data: GameSessionData) -> void:
-		var url := "%s/statistics/newSession" % baseUrl
-		data.gameId = gameId
+func initialize(_ApiKey: String, _gameId: String) -> void:
+	apiKey = _ApiKey
+	gameSessionData.gameId = _gameId
 
-		var request := HTTPRequest.new()
-		request.connect("request_completed", self, "_on_request_completed")
-		request.request(url, [], true, HTTPClient.METHOD_POST, JSON.print(data))
-		await(request, "completed")
+# Method to add a game session
+func send_session() -> void:
+	var url: String = "%s/statistics/newSession" % baseUrl
+	var headers: PackedStringArray = (["x-api-key: " + apiKey, "content-type: application/json"])
+	var body = JSON.stringify(gameSessionData.get_serializable_properties())
+	var request = HTTPRequest.new()
+	add_child(request)
+	request.request_completed.connect(self._http_request_completed)
+	
+	var error = request.request(url, headers, HTTPClient.METHOD_POST, body)
+	if error != OK:
+		push_error("An error occurred in the HTTP request. " + str(error))
 
-		if request.get_response_code() == 200:
-			var responseText := request.get_response_body_as_text()
-			print(responseText)
-		else:
-			print("Error: ", request.get_error())
+
+func _http_request_completed(result, response_code, headers, body):
+	if result != HTTPRequest.RESULT_SUCCESS:
+		push_error("Error: " + body)
+		return
+
+	var response_data = JSON.parse_string(body.get_string_from_utf8())
+	sessionId = response_data.sessionId
+
